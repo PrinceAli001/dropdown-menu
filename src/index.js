f3b55a50c33a7e16cafb03da8197cecd5f82a958
Mahfouz <mafz060706@gmail.com>
2024-07-20 18:32:42 +0100

    Designing in progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert('Yes');
+import './style.css';
+import Menu from '../Images/menu.svg';
+import Close from '../Images/close-thick.svg';
+
+display
+
+function display() {
+    let header = document.querySelector('header');
+    let background = document.querySelector('#background');
+    let menu = document.createElement('img');
+    menu.src = Menu;
+    header.textContent = '';
+    header.appendChild(menu)
+    header.appendChild(background);
+}
