2cad9deeca44f59b37a2656c5240c83d42d2def7
Mahfouz <mafz060706@gmail.com>
2024-07-21 18:21:05 +0100

    Design process nearing it's end

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import './style.css';
-import Menu from '../Images/menu.svg';
-import Close from '../Images/close-thick.svg';
 
 display
 
 function display() {
-    let header = document.querySelector('header');
-    let background = document.querySelector('#background');
-    let menu = document.createElement('img');
-    menu.src = Menu;
-    header.textContent = '';
-    header.appendChild(menu)
-    header.appendChild(background);
+   
 }
